8c13da99611cda3c19192fbed7484eb7e484afcb
Mudit Ameta <zeusdeux@gmail.com>
2019-12-29 06:09:03 +0100

    Build paths map for next.js from Contentful data

--- next.config.js
+++ next.config.js
@@ -1,17 +1,33 @@
 const got = require('got')
+const { createClient } = require('contentful')
+
+const ctfl = createClient({
+  space: 'pe315guv55pz',
+  accessToken: process.env.CDA_TOKEN
+})
 
 module.exports = {
-  exportTrailingSlash: true,
+  exportTrailingSlash: false,
   exportPathMap: async function() {
+    const posts = await ctfl.getEntries({
+      content_type: 'blogPost'
+    })
+
+    const pathsToPosts = posts.items.reduce((acc, post) => {
+      acc[`/posts/${post.fields.slug}`] = {
+        page: '/posts/[slug]',
+        query: { slug: post.fields.slug }
+      }
+      return acc
+    }, {})
     const paths = {
       '/': { page: '/' },
-      '/posts/first-post': { page: '/posts/[slug]', query: { slug: 'first-post' } },
-      '/posts/second-post': { page: '/posts/[slug]', query: { slug: 'second-post' } },
-      '/posts/third-post': { page: '/posts/[slug]', query: { slug: 'third-post' } }
+      ...pathsToPosts
     }
 
-    // generate more paths from blogs from contentful here,
-    // add it to the paths object above and return paths
+    console.log('*'.repeat(80))
+    console.log(paths)
+    console.log('*'.repeat(80))
 
     return paths
   }
