5de7545b27340cab85782ea127be10e93942c02c
Mudit Ameta <zeusdeux@gmail.com>
2020-01-19 12:18:09 +0100

    Add socials to the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,22 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaGithub, FaTwitter } from 'react-icons/fa'
 
 const Header = () => {
   return (
     <nav>
+      <a className="social-link" href="https://twitter.com/muditameta" rel="noopener noreferrer">
+        <FaTwitter className="social-icon" />
+      </a>
+
+      <a
+        className="social-link"
+        href="https://github.com/zeusdeux/blog.mudit.xyz"
+        rel="noopener noreferrer"
+      >
+        <FaGithub className="social-icon" />
+      </a>
+
       <a href="https://mudit.xyz" rel="noopener noreferrer" title="Mudit’s home page">
         mudit.xyz
       </a>
@@ -26,6 +39,14 @@ const Header = () => {
 
           nav > a:not(:last-child) {
             margin-right: 1rem;
+          }
+
+          nav > a.social-link {
+            display: flex;
+          }
+
+          nav > a.social-link > :global(.social-icon) {
+            align-self: flex-end;
           }
         `}
       </style>
