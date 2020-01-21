758ddec4b2b8b1b9605b90d2ec97123a715d5e0b
Mudit Ameta <zeusdeux@gmail.com>
2020-01-21 18:32:31 +0100

    Add collapsible table of contents

--- graphql/markdownToHtml.ts
+++ graphql/markdownToHtml.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import prism from '@mapbox/rehype-prism'
+import toc from '@zeusdeux/rehype-toc'
 // import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis'
 import link from 'rehype-autolink-headings'
 import raw from 'rehype-raw'
@@ -16,22 +17,39 @@ export default function markdownToHtml(md) {
       .use(markdown)
       .use(remark2rehype)
       .use(raw)
-      .use(urls, updateAnchorAndImgNode)
+      .use(urls, function updateAnchorAndImgNode(url, node) {
+        if (node.type === 'element' && node.tagName === 'a') {
+          node.properties.target = '_blank'
+          node.properties.rel = 'noopener noreferrer'
+        }
+        if (node.type === 'element' && node.tagName === 'img') {
+          node.properties.loading = 'lazy'
+        }
+      })
       // .use(rehypeAccessibleEmojis)
       .use(slug)
+      .use(toc, {
+        headings: ['h2', 'h3'],
+        position: 1,
+        customizeTOC(node) {
+          return {
+            type: 'element',
+            tagName: 'details',
+            properties: {},
+            children: [
+              {
+                type: 'element',
+                tagName: 'summary',
+                children: [{ type: 'text', value: 'Table of Contents' }]
+              },
+              node
+            ]
+          }
+        }
+      })
       .use(link, { behavior: 'wrap', properties: { className: 'permalink' } })
       .use(prism)
       .use(html)
       .process(md)
   )
-}
-
-function updateAnchorAndImgNode(url, node) {
-  if (node.type === 'element' && node.tagName === 'a') {
-    node.properties.target = '_blank'
-    node.properties.rel = 'noopener noreferrer'
-  }
-  if (node.type === 'element' && node.tagName === 'img') {
-    node.properties.loading = 'lazy'
-  }
 }
