5672056ce135721aab0c1a927eb5df1c19cc21ab
Mudit Ameta <zeusdeux@gmail.com>
2020-09-02 16:19:55 +0200

    Move redirects from now.json to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,5 +4,19 @@ module.exports = {
     CDA_TOKEN: process.env.CDA_TOKEN,
     PREVIEW_TOKEN: process.env.PREVIEW_TOKEN,
     SPACE_ID: process.env.SPACE_ID
+  },
+  async redirects() {
+    return [
+      {
+        source: '/posts',
+        destination: '/',
+        permanent: true
+      },
+      {
+        source: '/posts/',
+        destination: '/',
+        permanent: true
+      }
+    ]
   }
 }
